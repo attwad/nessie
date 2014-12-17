e2a2e33ec4264a5ccfadb1ddcd4ebe8411924112
attwad <userattwad@server.fake>
2014-12-17 10:32:11 +0900

    fixed build

--- nessie.go
+++ nessie.go
@@ -84,43 +84,6 @@ func (n *Nessus) doRequest(method string, resource string, data url.Values, want
 	return resp, nil
 }
 
-type loginResp struct {
-	Token string `json:"token"`
-}
-
-// ServerProperties is the structure returned by the ServerProperties() method.
-type ServerProperties struct {
-	Token           string `json:"token"`
-	NessusType      string `json:"nessus_type"`
-	NessusUIVersion string `json:"nessus_ui_version"`
-	ServerVersion   string `json:"server_version"`
-	Feed            string `json:"feed"`
-	Enterprise      bool   `json:"enterprise"`
-	LoadedPluginSet string `json:"loaded_plugin_set"`
-	ServerUUID      string `json:"server_uuid"`
-	Expiration      int64  `json:"expiration"`
-	Notifications   []struct {
-		Type string `json:"type"`
-		Msg  string `json:"message"`
-	} `json:"notifications"`
-	ExpirationTime int64 `json:"expiration_time"`
-	Capabilities   struct {
-		MultiScanner      bool `json:"multi_scanner"`
-		ReportEmailConfig bool `json:"report_email_config"`
-	} `json:"capabilities"`
-	PluginSet       string `json:"plugin_set"`
-	IdleTImeout     int64  `json:"idle_timeout"`
-	ScannerBoottime int64  `json:"scanner_boottime"`
-	LoginBanner     bool   `json:"login_banner"`
-}
-
-// ServerStatus is the stucture returned  by the ServerStatus() method.
-type ServerStatus struct {
-	Status             string `json:"status"`
-	Progress           int64  `json:"progress"`
-	MustDestroySession bool
-}
-
 // Login will log into nessus with the username and passwords given from the command line flags.
 func (n *Nessus) Login(username, password string) error {
 	log.Printf("Login into %s\n", n.apiURL)
