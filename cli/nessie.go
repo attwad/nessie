15671cb9e4cee1066388a9813a4bf54267d6ecf4
Daniel Dulay <daniel.dulay@gmail.com>
2017-04-03 14:54:27 +0200

    Fix fingerprints flag handling in cli.

--- cli/nessie.go
+++ cli/nessie.go
@@ -16,15 +16,17 @@ func init() {
 	flag.StringVar(&apiURL, "api_url", "", "")
 	flag.StringVar(&username, "username", "", "Username to login with, in production read that from a file, do not set from the command line or it will end up in your history.")
 	flag.StringVar(&password, "password", "", "Password that matches the provided username, in production read that from a file, do not set from the command line or it will end up in your history.")
-	flag.StringList(&fingerprints, nil, "SPKI Fingerprints for the Nessus server using SHA-256 encoded in base64.")
+	flag.StringVar(&fingerprints, "fingerprints", "", "Comma-separated list of SPKI Fingerprints for the Nessus server using SHA-256 encoded in base64.")
 	flag.Parse()
 }
 
 func main() {
-	if len(*fingerprints) > 0 {
-		nessus, err := nessie.NewFingerprintedNessus(apiURL, *fingerprints)
+	var err error
+	var nessus nessie.Nessus
+	if len(fingerprints) > 0 {
+		nessus, err = nessie.NewFingerprintedNessus(apiURL, strings.Split(fingerprints, ","))
 	} else {
-		nessus, err := nessie.NewInsecureNessus(apiURL)
+		nessus, err = nessie.NewInsecureNessus(apiURL)
 	}
 	if err != nil {
 		panic(err)
